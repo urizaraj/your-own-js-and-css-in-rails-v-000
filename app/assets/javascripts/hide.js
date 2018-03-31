6174c94733f9da0b2bc59b33a60a8ac2d2a1066b
Alexander Urizar <urizaraj@gmail.com>
2018-03-31 20:29:47 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,9 +1,10 @@
-const element = $('#hide_this')
+
 
 function hideWhenClicked() {
   element.hide()
 }
 
 $(document).ready(function() {
+  const element = $('#hide_this')
   element.on('click', hideWhenClicked)
 })
